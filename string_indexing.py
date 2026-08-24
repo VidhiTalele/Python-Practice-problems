dc95f051d85e66a6f3a7392df60338e5d7b8e920
Vidhi Talele <vidhitalele@gmail.com>
2026-08-24 16:48:32 +0530

    String Indexing and Even Slicing

--- string_indexing.py
+++ string_indexing.py
@@ -1,2 +1,22 @@
+#Solution 1
 string = input("Enter a string:")
 print(string[::2])
+
+#or
+
+
+#Solution 2
+string = input("Enter a string:")
+n = len(string)
+
+for i in range(0,n,2):
+    print(string[i])
+
+#or
+
+#Solution 3
+string = input("Enter a string:")
+n = len(string)
+for i in range(n):
+    if i%2 == 0:
+        print(string[i])
